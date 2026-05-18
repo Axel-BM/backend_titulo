0a60aa902c7fa7c95827d9b92047ace92e09ca08
Axel-BM <2025.estudiando@gmail.com>
2026-05-18 00:24:11 -0400

    Rectificacion de CORS

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const routes = require("./routes/index.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
